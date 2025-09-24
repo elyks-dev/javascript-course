db05cc71e89678b07d1856f0f0b6082d86c54673
elyks-dev <elyksalonzo@gmail.com>
2025-09-24 13:15:39 +0800

    feat(behind-the-scenes): complete Hour 2 with this keyword and arrow functions
    - Add this binding rules for method calls and detached functions
    - Show arrow functions vs regular functions with lexical this behavior
    - Solve common this pitfalls using arrow functions and self = this patterns
    - Compare arguments object in regular functions vs rest parameters
    - Add practical timer examples showing callback this problems and solutions
    - Include comprehensive this binding examples and best practices
    - Master dynamic vs lexical this binding in JavaScript

--- 07-behind-the-scenes/starter/script.js
+++ 07-behind-the-scenes/starter/script.js
@@ -1,6 +1,7 @@
-'use strict';
+// 'use strict';
+// person.greet();
 
-console.log(varX);
+// console.log(varX);
 // console.log(letX);
 // console.log(constX);
 
@@ -66,3 +67,128 @@ console.log(varX);
 // };
 
 // const addArrow = (a, b) => a + b;
+
+// const person = {
+//   name: 'Jonas',
+//   greet: function () {
+//     console.log(`Hello, I am ${this.name}`);
+//   },
+// };
+
+// person.greet();
+// // person.name = 'Kyle';
+// // person.greet();
+
+// const anotherPerson = {
+//   name: 'Sarah',
+// };
+
+// anotherPerson.great = person.greet;
+// anotherPerson.greet();
+
+// //
+// const greetFunction = person.greet;
+// greetFunction();
+
+// // This breaks in event handlers
+// const button = document.querySelector('button');
+// button.addEventListener('click', person.greet); // `this` becomes the button element!
+
+// // Fix: use arrow function wrapper
+// button.addEventListener('click', () => person.greet());
+// // Or bind the method
+// button.addEventListener('click', person.greet.bind(person));
+
+// const obj = {
+//   name: 'Object',
+
+//   regularMethod: function () {
+//     console.log('Regular:', this.name);
+//   },
+
+//   arrowMethod: () => {
+//     console.log('Arrow:', this.name);
+//   },
+// };
+
+// obj.regularMethod();
+// obj.arrowMethod();
+
+// const quiz = {
+//   name: 'Quiz Object',
+//   regularMethod() {
+//     console.log('Regular:', this.name);
+//   },
+//   arrowMethod: () => {
+//     console.log('Arrow:', this.name);
+//   },
+// };
+
+// quiz.regularMethod(); // Predict: ?
+// quiz.arrowMethod(); // Predict: ?
+
+// const timer = {
+//   name: 'Timer',
+//   // Using the old ways
+//   start: function () {
+//     console.log(`${this.name} starting...`);
+//     const self = this;
+
+//     setTimeout(function () {
+//       console.log(`${self.name} finished`);
+//     }, 1000);
+//   },
+//   // Using arrow function
+//   startModern: function () {
+//     console.log(`${this.name} starting modern...`);
+
+//     setTimeout(() => {
+//       console.log(`${this.name} finished modern`);
+//     });
+//   },
+// };
+
+// timer.start();
+// timer.startModern();
+
+// const user = {
+//   name: 'Alice',
+//   hobbies: ['reading', 'coding', 'gaming'],
+
+//   // BROKEN: arrow function loses `this`
+//   printHobbiesBad: () => {
+//     this.hobbies.forEach(hobby => {
+//       console.log(`${this.name} likes ${hobby}`);
+//     });
+//   },
+
+//   // FIXED: regular function preserves `this`
+//   printHobbiesGood() {
+//     this.hobbies.forEach(hobby => {
+//       console.log(`${this.name} likes ${hobby}`);
+//     });
+//   },
+// };
+
+// // user.printHobbiesBad();
+// user.printHobbiesGood();
+
+// const functionTypes = {
+//   regularFunction: function () {
+//     console.log('Arguments length:', arguments.length);
+//     console.log('First argument:', arguments[0]);
+//   },
+
+// arrowFunction: () => {
+//     console.log('Arrow function called');
+//   },
+
+//   modernFunction: (...args) => {
+//     console.log('Args length:', args.length);
+//     console.log('First arg:', args[0]);
+//   },
+// };
+
+// functionTypes.regularFunction('hello', 'world');
+// // functionTypes.arrowFunction('test');
+// functionTypes.modernFunction('modern', 'approach');
